3c16cbce804384be1c1e2123ee2fb2401802f86e
jeftersantiago <jeftersmares@gmail.com>
2022-09-25 02:18:56 -0300

    comments

--- trabalhos/catalogo_de_vinhos/search.c
+++ trabalhos/catalogo_de_vinhos/search.c
@@ -1,13 +1,10 @@
 #include "search.h"
 #include "wine.h"
-
 #include <stdio.h>
-
 /**
    Verifique se o valor encontrado é o primeiro dentre
    todos os outros iguais no vetor, caso contrário,
    busque o primeiro deles.
-
 **/
 Wine *binarySearch(Wine **list, const Wine *key, int begin, int end, const int property){
 //     insertionSort(list, N_ROWS, property);
@@ -24,12 +21,27 @@ Wine *binarySearch(Wine **list, const Wine *key, int begin, int end, const int p
      }
      return emptyWine(); 
 }
-
+/**
+   Ordenação descrita nas especificações do trabalho.
+   Parameters:
+   Wine **list: list of structs of wines.
+   int size: size of list of structs. 
+   int property: attribute we want to sort.
+      1 - citric_acid
+      2 - residual_sugar
+      3 - density
+      4 - ph
+      Other - alcohol
+ **/
 void wineSort(Wine **list, const int size, const int property){
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size - i; j++){
             if(getWine(list[j], property) > getWine(list[i], property))
                 swap(list, j, i);
+            /**
+               Check the cases where the attribute value is equal and
+               if it is, sort it by ID in increasing order.
+             **/
             if((getWine(list[j], property) == getWine(list[i], property)) && getId(list[j]) > getId(list[i]))
                 swap(list, i, j);
         }
@@ -42,21 +54,15 @@ void swap(Wine **list, int currentIndex, int newIndex){
 }
 
 /**
-   Talvez
-   void mergeSort(Wine **list);
-**/
+   ## DEBUG 
 
-/**
-   Parametros:
+   Parameters:
    int property:
       1 - citric_acid
       2 - residual_sugar
       3 - density
       4 - ph
       Other - alcohol
-
-   Nao foi necessario usar o criterio de desempate pelo ID
-   pois os dados já vem ordenados em relação a esta propriedade.
 **/
 void insertionSort(Wine **list, const int listSize, const int property){
     for(int i = 1; i < listSize; i++){
